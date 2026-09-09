3935f488eba21de9cc6347397341e69ea18a898a
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-09 22:53:23 +0530

    Update flight.cpp

--- src/flight.cpp
+++ src/flight.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "../include/Flight.h"
 
+using namespace std;
+
+int main(){
+
+vector < Flight> flights;
+
+  Flight f1;
+  f1.flightnumber = "AV1";
+  f1.
+
+
+
+
+
+}
