40b3eea3582b861efd186e407a46b8c6ed43eaa2
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-10 01:08:46 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,19 @@ int main (){
 
   vector <passenger> passengers;
 
-  passenger p1("rajat", 19, )
+  passenger p1("rajat", 19, 8077016775);
+  passengers.push_back(p1);
+
+  passenger p2("Naman", 25, 8077016700);
+  passengers.push_back(p2);
+
+  passenger p3("Deepak", 23, 9897272454);
+  passengers.push_back(p3);
+
+  passenger p4("Rohit", 27, 8077099915);
+  passengers.push_back(p4);
+
+  string name;
 
 }
 
