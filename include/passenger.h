a7845518c0fb1e91626d87486f9eceec4cfaadab
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-10 00:51:18 +0530

    Define Passenger class with constructor and display method

--- include/passenger.h
+++ include/passenger.h
@@ -1,1 +1,26 @@
+#ifndef PASSENGER_H
+#define PASSENGER_H
 
+passenger{
+
+string name;
+int age;
+string phone;
+
+passenger( string n, int a, string p){
+
+name = n;
+age = a;
+phone = p;
+  
+}
+void display(){
+
+cout << "Name: "<< name;
+cout << "age: " << age;
+cout << "Phone: " << phone;
+  
+}
+
+
+};
