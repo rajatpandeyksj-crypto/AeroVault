5600312eae17a2a98b20093e2563181a695c46d0
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-09 22:49:48 +0530

    Update flight.h

--- include/flight.h
+++ include/flight.h
@@ -1,1 +1,25 @@
+#ifndef FLIGHT_H
+#define FLIGHT_H
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Flight{
+public:
+
+string flightnumber;
+string source;
+string destination;
+int price;
+int seats;
+
+void display(){
+cout << "flightnumber: "<<flightnumber<< endl;
+cout << "Source: "<< source << endl;
+cout << "Destination: "<< destination << endl;
+cout << "Price: "<< price << endl;
+cout << "Seats: "<< seats << endl;
+}
+};
+
+#endif
