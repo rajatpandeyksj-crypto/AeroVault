bb33885166981d43bfb14f9f727304a2a0f2ccaa
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-17 14:34:50 +0530

    Update reservation.cpp

--- src/reservation.cpp
+++ src/reservation.cpp
@@ -1,64 +1,18 @@
+#include "reservation.h"
 #include "flight.h"
 #include "passenger.h"
-#include "reservation.h"
 
-void makeReservation(vector<Flight>& flights, vector<passenger>& passengers) {
+void makeReservation(Flight& flight, passenger& p) {
 
-    string name;
-    string number;
+    if (flight.bookseat()) {
 
-    cout << "\nEnter Passenger Name: ";
-    cin >> name;
+        Reservation r1(p.name, flight.flightnumber);
 
-    bool passengerFound = false;
+        r1.display();
 
-    for (int i = 0; i < passengers.size(); i++) {
-
-        if (name == passengers[i].name) {
-
-            passengerFound = true;
-
-            cout << "\nPassenger Found!" << endl;
-            passengers[i].display1();
-
-            break;
-        }
+        cout << "Seats Left: " << flight.seats << endl;
     }
-
-    if (!passengerFound) {
-        cout << "\nPassenger not found." << endl;
-        return;
-    }
-
-    cout << "\nEnter Flight Number: ";
-    cin >> number;
-
-    bool flightFound = false;
-
-    for (int i = 0; i < flights.size(); i++) {
-
-        if (number == flights[i].flightnumber) {
-
-            flightFound = true;
-
-            if (flights[i].bookseat()) {
-
-                Reservation r1(name, number);
-
-                r1.display();
-
-                cout << "Seats Left: "
-                     << flights[i].seats << endl;
-            }
-            else {
-                cout << "\nNo seats left." << endl;
-            }
-
-            break;
-        }
-    }
-
-    if (!flightFound) {
-        cout << "\nFlight not found." << endl;
+    else {
+        cout << "No seats left." << endl;
     }
 }
