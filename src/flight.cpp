a6d629a411529fa9b8efba106c0b1f5711b8fa8c
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-17 13:36:36 +0530

    Update flight.cpp

--- src/flight.cpp
+++ src/flight.cpp
@@ -4,53 +4,50 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-vector < Flight> flights;
+    vector<Flight> flights;
 
-  Flight f1("AV1", "DELHI", "BANGLORE", 5500, 40);
-  flights.push_back(f1);
+    Flight f1("AV1", "DELHI", "BANGLORE", 5500, 40);
+    flights.push_back(f1);
 
-  Flight f2("AV2", "CHENNAI", "BANGLORE", 6500, 50);
-  flights.push_back(f2);
+    Flight f2("AV2", "CHENNAI", "BANGLORE", 6500, 50);
+    flights.push_back(f2);
 
-  Flight f3("AV3", "DEHRADUN", "CHENNAI", 5000, 60);
-  flights.push_back(f3);
+    Flight f3("AV3", "DEHRADUN", "CHENNAI", 5000, 60);
+    flights.push_back(f3);
 
-  Flight f4("AV4", "ODISHA", "BANGLORE", 7000, 45);
-  flights.push_back(f4);
+    Flight f4("AV4", "ODISHA", "BANGLORE", 7000, 45);
+    flights.push_back(f4);
 
-  Flight f5("AV5", "DELHI", "HYDERABAD", 3500, 20);
-  flights.push_back(f5);
+    Flight f5("AV5", "DELHI", "HYDERABAD", 3500, 20);
+    flights.push_back(f5);
 
+    string number;
+    int n = flights.size();
 
-  string number;
-int n = flights.size ();
- 
     cout << "============================" << endl;
     cout << "         AEROVAULT" << endl;
     cout << "============================" << endl;
 
-  cout << "\n Enter The Flight Number: ";
-cin >> number;
+    cout << "\nEnter The Flight Number: ";
+    cin >> number;
 
-  bool found = false;
+    bool found = false;
 
-  for (int i =0; i < n; i++){
-  if (number == flights[i].flightnumber){
-   found = true;
-    flights[i].display();
-    break;
-  
-  }
-  }
+    for (int i = 0; i < n; i++) {
 
-  
-  }
-     if (!found) {
+        if (number == flights[i].flightnumber) {
+
+            found = true;
+            flights[i].display();
+            break;
+        }
+    }
+
+    if (!found) {
         cout << "\nFlight not found." << endl;
     }
 
-  return 0;
-    
+    return 0;
 }
