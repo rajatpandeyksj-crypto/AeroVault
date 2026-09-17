1b6d8045b1cf44243029aab5f04dfdbb7a834b52
rajat pandey <rajatpandeyksj@gmail.com>
2026-09-17 13:07:07 +0530

    Update reservation.h

--- include/reservation.h
+++ include/reservation.h
@@ -1,1 +1,25 @@
+#ifndef RESERVATION_H
+#define RESERVATION_H
 
+#include <iostream>
+#include <string>
+
+using namepsace std;
+
+class Reservation{
+public:
+string passengerName;
+string flightNumber;
+
+resrevation(string name, string number){
+passengerName = name;
+  flightNumber = number;
+}
+
+void display(){
+  cout << "Passenger Name: " << passengerName << endl;
+  cout << "Flight Number: " << flightNumber << endl;
+  cout << "Reservation Confirmed" << endl;
+}
+
+};
